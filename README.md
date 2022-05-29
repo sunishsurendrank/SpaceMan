bd1575e1376d6d1b95616f8ec52e6f1d18aeaf3e
Sunish Surendran K <sunishsurendrank@gmail.com>
2022-05-29 11:59:08 +0200

    Adding the python script

---
+++ README.md
@@ -0,0 +1,32 @@
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) [![contributions welcome](https://img.shields.io/static/v1.svg?label=Contributions&message=Welcome&color=0059b3&style=flat-square)](https://github.com/TheAlgorithms/Python/blob/master/CONTRIBUTING.md)&nbsp;
+
+
+
+# SpaceMan Game
+
+This game is developed in python using the library pygame.Inspired from Flappy Bird game in android.
+
+- Author of this Document : Sunish Surendran Kannembath
+- Developer : Sunish Surendran Kannembath
+- Reach Sunish in LinkedIn : https://www.linkedin.com/in/sunishsurendrank/
+- Reach Sunish in Twitter : @sunishsurendran
+
+# How to play SpaceMan in your machine
+
+### Step 1: 
+Clone this repository to your desktop
+
+### Step 2:
+Install latest version of python in your machine.
+
+### Step 3: 
+Using pip install pygames
+
+`pip install pygames`
+
+### Step 4:
+Go to the folder of this repository and open a command promopt in your machine and run the script.
+
+`python.exe .\spaceman.py`
+
+
---
+++ spaceman.py
@@ -0,0 +1,100 @@
+import random
+import pygame
+import time
+import sys
+
+"""
+- Developer : Sunish Surendran Kannembath
+- Reach Sunish in LinkedIn : https://www.linkedin.com/in/sunishsurendrank/
+- Reach Sunish in Twitter : @sunishsurendran
+"""
+
+
+def CreatePipe():
+    global score
+    gap = 300
+    pipe_color = (127,127,127)#(0,255,0)
+    pipe_width = 50
+    pygame.draw.rect(window,pipe_color,(pipe[0], 0, pipe_width, pipe[1]))
+    pygame.draw.rect(window,pipe_color,(pipe[0], pipe[1]+gap, pipe_width, 720))
+    pipe[0] = pipe[0] - 5
+    if pipe[0] < -50:
+        pipe[0] = 720
+        pipe[1] = random.randint(0,380)
+        score = score + 1
+
+def SpaceBarEvent():
+    global jump,start,y_axis,start
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                jump = 7
+                if start == False:
+                    y_axis = 400
+                    start = True
+                    time.sleep(1)
+
+def Character():
+    global y_axis,jump
+    global window
+    global character_image,start
+    window.blit(character_image,(50,y_axis))
+    y_axis = y_axis - jump
+    jump = jump - 0.5
+
+def HitCondition():
+    global y_axis,pipe
+    if (pipe[0] < 164 and pipe[0] > 14) and (y_axis+100 > pipe[1]+300 or y_axis < pipe[1]):
+        return True
+
+
+if __name__ == "__main__":
+
+
+    pygame.init()
+    window              = pygame.display.set_mode((620,620))
+    clock               = pygame.time.Clock()
+    pipe                = [620,random.randint(0,380)]
+    y_axis              = 100
+    jump                = 0
+    score               = 0
+    start               = True
+    stop                = False
+    title_font          = pygame.font.SysFont('Arial', 50)
+    message_font        = pygame.font.SysFont('Arial', 20)
+    score_font          = pygame.font.SysFont('Arial', 20)
+    character_image     = pygame.image.load("./images/character.png")
+    gameover_image      = pygame.image.load("./images/gameover.png")
+    background_image    = pygame.image.load("./images/background.jpg")
+
+    while True:
+        #check any spacebar press event happned
+        SpaceBarEvent()
+        
+        #Start the Game
+        if start:
+
+            window.fill((120,120,255))
+            window.blit(background_image,(0,0))
+            Character()
+            CreatePipe()
+            window.blit(score_font.render('Score: ' + str(score), True, (255,255,255), None),(10,10))
+
+        #Check Whether the charchter hit the pipe
+        retrun_value = HitCondition()
+
+        #If the character hit the pipe then show gameover
+        if retrun_value:
+            window.blit(gameover_image,(160,160))
+            caption = message_font.render('Press spacebar to continue playing', True, (255,255,255), None)
+            window.blit(caption,(180,320))
+            start   = False
+            stop    = True
+            pipe[0] = 720
+
+        #clock tick decised the frames per second
+        clock.tick(60)
+        pygame.display.flip()
